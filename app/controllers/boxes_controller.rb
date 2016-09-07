14cd320de234d3fc616f30192d1977842bb6a590
Chase <chasefreeman13@gmail.com>
2016-09-07 13:23:11 -0400

    added user filters for box and recipe show and index pages

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -1,5 +1,5 @@
 class BoxesController < ApplicationController
-before_action :authenticate_user!
+ before_filter :authenticate_user!, except: [ :index, :show ]
 
     def index
       @boxes = Box.all
